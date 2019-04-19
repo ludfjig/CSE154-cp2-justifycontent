7f1444561c59e80f4a01f2b78c3318b37b42d614
ludfjig <ludvil@uw.edu>
2019-04-19 11:54:09 -0700

    done except for init() comment in main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-// Name: _your name here_
-// Date: _add date here_
-// Section: CSE 154 _your section here_
+// Name: Ludvig Liljenberg
+// Date: 04/19/2019
+// Section: CSE 154 AF
 //
-// -- your description of what this file does here --
-//
+// This script adds functionality to a dropdown and a
+// button in index.html. It specifically allowes the user to change
+// the css property "justify-content" in a div on the site. It also allowes
+// the user to add "boxes" to the div with a press of a button.
 
 (function() {
   "use strict";
@@ -11,23 +13,31 @@
   window.addEventListener("load", init);
 
   /**
-   *  CHANGE: Describe what your init function does here.
+   *  Adds functionality to the button and dropdown menu on index.html
    */
   function init() {
     document.getElementById("justifier").addEventListener("change", changeCss);
     document.querySelector("button").addEventListener("click", addBox);
   }
 
+  /**
+   * Adds a new "box" html element, as a child of div #container
+   */
   function addBox() {
     let newBox = document.createElement("div");
     newBox.className = "box";
     document.getElementById("container").appendChild(newBox);
   }
 
-  function changeCss (){
+  /**
+   * Changes the css property "justify-content" of the div #container
+   * based on the selected vale in the dropdown menu
+   */
+  function changeCss() {
     let d = document.getElementById("container");
-    d.classList = "";
-    d.classList.add(this.value);
+    // replace the current class with the class with same name as the selected
+    // option in the dropdown menu value
+    d.className = this.value;
   }
 
 })();
